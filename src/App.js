504cb0d98e8b707169af072dc5ff57faaab1161a
amstex <amstex2007@gmail.com>
2025-06-18 10:24:36 +0600

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Comp/Navbar';
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
